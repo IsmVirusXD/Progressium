114370b5f88c1dc504d115944ef135250803beed
IsmVirusXD <igordesouzamendes@outlook.com>
2026-06-05 22:18:26 -0300

    refactor(ui): change MAP configuration

--- app/ui/textButton.tsx
+++ app/ui/textButton.tsx
@@ -1,19 +1,28 @@
-"use client";
-
 import { twJoin } from "tailwind-merge";
 import { typography } from "../styles/fonts/typografy";
-
-type buttonSize = "medium" | "small";
-type buttonType = "confirm" | "cancel" | "text";
 
 interface TextButtonProps {
   text: string;
   activate: boolean;
   onClick: () => void;
-  size: buttonSize;
-  type: buttonType;
+  size: "medium" | "small";
+  type: "confirm" | "cancel" | "text";
   cyId: string;
 }
+
+const COLOR_MAP = {
+  confirm:
+    "text-done border border-done hover:border-0 hover:bg-done hover:text-background",
+  cancel:
+    "text-secondary border border-secondary hover:border-0 hover:bg-secondary hover:text-background",
+  text: "text-primary border border-primary hover:border-0 hover:bg-primary hover:text-background",
+  deactivate: "border-0 bg-muted text-background opacity-30",
+};
+
+const SIZE_MAP = {
+  medium: "w-40 h-9",
+  small: "w-28 h-9",
+};
 
 export function TextButton({
   text,
@@ -23,24 +32,11 @@ export function TextButton({
   type,
   cyId,
 }: TextButtonProps) {
+  const id = `${cyId}-text-button`;
   const baseStyle = "h-9 border text-center rounded-md";
 
-  const colorStyles = {
-    confirm:
-      "text-done border border-done hover:border-0 hover:bg-done hover:text-background",
-    cancel:
-      "text-secondary border border-secondary hover:border-0 hover:bg-secondary hover:text-background",
-    text: "text-primary border border-primary hover:border-0 hover:bg-primary hover:text-background",
-    deactivate: "border-0 bg-muted text-background opacity-30",
-  };
-
-  const componentSize = {
-    medium: "w-40 h-9",
-    small: "w-28 h-9",
-  };
-
   const tailwindConfig: string = [
-    componentSize[size],
+    SIZE_MAP[size],
     baseStyle,
     typography.button,
   ].join(" ");
@@ -49,10 +45,10 @@ export function TextButton({
     <button
       className={twJoin(
         tailwindConfig,
-        activate ? colorStyles[type] : colorStyles["deactivate"],
+        activate ? COLOR_MAP[type] : COLOR_MAP["deactivate"],
       )}
       onClick={onClick}
-      id={`${cyId}-text-button`}
+      id={id}
     >
       {text}
     </button>
