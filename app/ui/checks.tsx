bd55f25007d19db7a7cc8e907beee9daa922a795
IsmVirusXD <igordesouzamendes@outlook.com>
2026-06-05 22:17:15 -0300

    feat(ui): add new checks component

---
+++ app/ui/checks.tsx
@@ -0,0 +1,57 @@
+import {
+  Circle,
+  CircleCheckBig,
+  CircleX,
+  Square,
+  SquareCheckBig,
+  SquareX,
+} from "lucide-react";
+
+interface CheckProps {
+  mode: "square" | "circle";
+  isDone: boolean;
+  isErro?: boolean;
+  onClick: () => void;
+  cyId: string;
+}
+
+const ICON_MAP = {
+  circle: { default: Circle, done: CircleCheckBig, error: CircleX },
+  square: { default: Square, done: SquareCheckBig, error: SquareX },
+};
+
+export function Checker({
+  mode,
+  isDone,
+  isErro = false,
+  onClick,
+  cyId,
+}: CheckProps) {
+  const id = `${cyId}-${mode}-checker`;
+  const iconStyle = "h-5 w-5";
+  const colorStyle = isErro
+    ? "text-muted"
+    : isDone
+      ? "text-done"
+      : "text-secondary";
+
+  const tailwindConfig: string = [iconStyle, colorStyle].join(" ");
+
+  const iconConfig = ICON_MAP[mode];
+  const IconComponent = isErro
+    ? iconConfig.error
+    : isDone
+      ? iconConfig.done
+      : iconConfig.default;
+
+  return (
+    <button
+      onClick={!isErro ? onClick : undefined}
+      id={isErro ? `${id}-error` : id}
+      disabled={isErro}
+      className={tailwindConfig}
+    >
+      <IconComponent />
+    </button>
+  );
+}
